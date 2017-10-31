5e50a384e2e775308e0e9be8c6266a222a63bc17
frenetic <frenetic@ubuntu-1404.vagrantup.com>
2017-10-31 19:20:00 +0000

    log

--- src/main/java/net/floodlightcontroller/automaniot/IoTRouting.java
+++ src/main/java/net/floodlightcontroller/automaniot/IoTRouting.java
@@ -1424,7 +1424,6 @@ public class IoTRouting implements IOFIoTRouting, IFloodlightModule, IOFMessageL
 
 */
 			Path newPath = getLowerPathLatency(appReq);
-			log.info("newPath {}", newPath.toString());
 			if(newPath.getLatency().getValue() < appReq.getMax()){ //TODO: Verificar a eficiencia
 				boolean result = pushRoute(newPath, appReq);
 				if (result) {
